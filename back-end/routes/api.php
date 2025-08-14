260a614e337503b5ff72dc294c76f39ce7b3b62d
Andriy Gnap <dghjjkk63@gmail.com>
2025-08-14 12:35:06 +0300

    Added CategoryController

--- back-end/routes/api.php
+++ back-end/routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\V1\HomeController;
+use App\Http\Controllers\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,5 +10,9 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::prefix('v1')->group(function () {
-    Route::get('/home', [App\Http\Controllers\Api\V1\HomeController::class, 'index']);
+    Route::get('/home', [HomeController::class, 'index']);
+});
+
+Route::prefix('v1')->group(function (){
+    Route::get('/getCategory', [CategoryController::class, 'index']);
 });
